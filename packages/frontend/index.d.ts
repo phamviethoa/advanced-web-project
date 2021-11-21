f9fa1c82cc073a22b23789b4cbc697f1a25bd8da
Pham Viet Hoa <hoapham@kms-technology.com>
2021-11-21 14:32:54 +0700

    feat: add frontend login with credential

--- packages/frontend/index.d.ts
+++ packages/frontend/index.d.ts
@@ -1,3 +1,4 @@
 declare module 'next-auth/providers/facebook';
 declare module 'next-auth/providers/google';
+declare module 'next-auth/providers/credentials';
 declare module 'bootstrap/dist/js/bootstrap';
--- packages/frontend/pages/api/auth/[...nextauth].ts
+++ packages/frontend/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,9 @@
-import NexAuth from 'next-auth';
+import NextAuth from 'next-auth';
 import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
-export default NexAuth({
+export default NextAuth({
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_ID,
@@ -11,6 +12,30 @@ export default NexAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'Username',
+          type: 'text',
+          placeholder: 'Your Email',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Your Password',
+        },
+      },
+      async authorize(credentials: Record<string, string>, req: any) {
+        const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
     }),
   ],
 });
