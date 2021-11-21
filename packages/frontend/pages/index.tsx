3e17248dd07f7221d0b0388172cb03822d031278
Pham Viet Hoa <hoapham@kms-technology.com>
2021-11-21 14:59:24 +0700

    fix: solve conflict

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Link from 'next/link';
-import { signIn, signOut, useSession } from 'next-auth/client';
-
-const Home = () => {
-  const [session, loading] = useSession();
-
-  if (session) {
-    console.log(session);
-  } else {
-    console.log('You are not sign in');
-  }
-
-  const login = () =>
-    signIn('login', {
-      name: 'J Smith',
-      email: 'jsmith@example.com',
-    });
-
-  return (
-    <div>
-      <button onClick={login}>Sign In</button>
-      <button>Sing out</button>
-    </div>
-=======
 import Layout from 'components/Layout';
 
 const Home = () => {
@@ -31,7 +5,6 @@ const Home = () => {
     <Layout>
       <p className="mt-5">Welcome to classroom</p>
     </Layout>
->>>>>>> 3db4dbc983c9c278523e7a12df37c2cc7933c6d5
   );
 };
 
